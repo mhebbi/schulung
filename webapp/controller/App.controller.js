b7dcb6689d0d05efc9bc94fe4a62a7f9800805e3
mhebbi <mihi@hebgen.de>
2017-10-18 16:49:05 +0200

    Tag1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,50 +1,56 @@
 sap.ui.controller("controller.App", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf controller.App
-*/
-//	onInit: function() {
-//
-//	},
+	/**
+	 * Called when a controller is instantiated and its View controls (if available) are already created.
+	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+	 * @memberOf controller.App
+	 */
+		onInit: function() {
+			var oData = {
+				value1: 50,
+				value2: 75
+			};
+			
+	  var oModel = new sap.ui.model.json.JSONModel( oData );
+      sap.ui.getCore().setModel(oModel,"dataModel");
+		},
+	/**
+	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+	 * (NOT before the first rendering! onInit() is used for that one!).
+	 * @memberOf controller.App
+	 */
+	//	onBeforeRendering: function() {
+	//
+	//	},
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf controller.App
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+	/**
+	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+	 * This hook is the same one that SAPUI5 controls get after being rendered.
+	 * @memberOf controller.App
+	 */
+/*	onAfterRendering: function() {
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf controller.App
+	},
 */
-//	onAfterRendering: function() {
-//
-//	},
+	/**
+	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+	 * @memberOf controller.App
+	 */
+	//	onExit: function() {
+	//
+	//	}
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf controller.App
-*/
-//	onExit: function() {
-//
-//	}
-				
-onButtonPressed: function ( ) { alert( "Bitte nicht drücken");
-												 },
-				
- onSliderChanged: function( oEvent ) { 
- 	    			alert( "Du hast den " +
-					oEvent.getSource().getId() +
-					" auf " +
-					oEvent.getSource().getValue() +
-					" gestellt."
-					);
-				}							
+	onButtonPressed: function() {
+		alert("Bitte nicht drücken");
+	},
+
+	onSliderChanged: function(oEvent) {
+		alert("Du hast den " +
+			oEvent.getSource().getId() +
+			" auf " +
+			oEvent.getSource().getValue() +
+			" gestellt."
+		);
+	}
 
 });
--- webapp/view/App.view.js
+++ webapp/view/App.view.js
@@ -1,48 +1,62 @@
 sap.ui.jsview("view.App", {
 
 	/** Specifies the Controller belonging to this View. 
-	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf view.App
-	*/ 
-	getControllerName : function() {
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf view.App
+	 */
+	getControllerName: function() {
 		return "controller.App";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf view.App
-	*/ 
-	createContent : function(oController) {
-		var oSlider1 = new sap.m.Slider( "Slider1", {
-						    change: function() {
-							alert( "Der Slider steht auf:" + oSlider1.getValue()); }
-							});
-			 							
-	    var oSlider2 = new sap.m.Slider( "Slider2", {
-												change: oController.onSliderChanged
-												}
-												);	
-			 
-			 var oText1 = new sap.m.Text({ text: "Dieses ist falsch"
-										}
-										);
-			
-			 var oButton = new sap.m.Button( {
-			                                   tooltip: "Dies ist kein Knopf sondern ein Button",
-											   text: "Knopf",
-											   press: oController.onButtonPressed
-											   }
-											   );
+	 * Since the Controller is given to this method, its event handlers can be attached right away. 
+	 * @memberOf view.App
+	 */
+	createContent: function(oController) {
+		var oSlider1 = new sap.m.Slider("Slider1", {
+			value: "{dataModel>/value1}",
+			change: function() {
+				alert("Der Slider steht auf:" + oSlider1.getValue());
+			}
 		
+		});
+		var oText1 = new sap.m.Text({
+			text: "{path: 'dataModel>/value1'}"
+		});
+
+		var oSlider2 = new sap.m.Slider("Slider2", {
+			value: "{model: 'dataModel', path: '/value2'}",
+			change: oController.onSliderChanged
+		});
+
+		var oText2 = new sap.m.Text({
+			text: "{path: 'dataModel>/value2'}"
+		});
+
+		var oButton = new sap.m.Button({
+			tooltip: "Dies ist kein Knopf sondern ein Button",
+			text: "Knopf",
+			press: oController.onButtonPressed
+		});
 		
- 		return new sap.m.Page({
-			title: "Title",
-			content: [ 
-				oSlider1, 
-				oSlider2,
+		var oVLayout = new sap.m.VBox({
+		//	alignItems: "Center",
+/*			justifyContent: sap.m.FlexJustifyContent.SpaceAround,
+			alignItems: sap.m.FlexAlignItems.Center,*/
+			items: [
+				oSlider1,
 				oText1,
+				oSlider2,
+				oText2,
 				oButton
 			]
+		});
+
+		return new sap.m.Page({
+			title: "Title",
+			content:[
+				 oVLayout
+				]
 		});
 	}
 
