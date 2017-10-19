85d98a218d4372a61b4d7a32df8a62a77382ae77
mhebbi <mihi@hebgen.de>
2017-10-19 09:37:20 +0200

    XML erweitert < macht Schwierigkeiten

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,15 +5,15 @@ sap.ui.controller("controller.App", {
 	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 	 * @memberOf controller.App
 	 */
-		onInit: function() {
-			var oData = {
-				value1: 50,
-				value2: 75
-			};
-			
-	  var oModel = new sap.ui.model.json.JSONModel( oData );
-      sap.ui.getCore().setModel(oModel,"dataModel");
-		},
+	onInit: function() {
+		var oData = {
+			value1: 50,
+			value2: 75
+		};
+
+		var oModel = new sap.ui.model.json.JSONModel(oData);
+		sap.ui.getCore().setModel(oModel, "dataModel");
+	},
 	/**
 	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 	 * (NOT before the first rendering! onInit() is used for that one!).
@@ -28,10 +28,10 @@ sap.ui.controller("controller.App", {
 	 * This hook is the same one that SAPUI5 controls get after being rendered.
 	 * @memberOf controller.App
 	 */
-/*	onAfterRendering: function() {
+	/*	onAfterRendering: function() {
 
-	},
-*/
+		},
+	*/
 	/**
 	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 	 * @memberOf controller.App
@@ -44,7 +44,7 @@ sap.ui.controller("controller.App", {
 		alert("Bitte nicht drücken");
 	},
 
-	onSliderChanged: function(oEvent) {
+	onSliderChange: function(oEvent) {
 		alert("Du hast den " +
 			oEvent.getSource().getId() +
 			" auf " +
